ee494390ba4a9ae7c0a3dece6e392ba4288cb5a9
fiamma66 <fiamma66@gmail.com>
2019-02-06 12:22:21 +0800

    Update Method.py

--- crawler_code/Method.py
+++ crawler_code/Method.py
@@ -1,12 +1,17 @@
 # use in crawler
+import requests
+from bs4 import BeautifulSoup
 
 
-def get_pixnet_content(soup):
+def get_pixnet_content(url):
 
     # 傳入 經過BeautifulSoup 處理的 soup
     # 若出現亂碼情況 請在requests.get()之後
     # 加入encoding = 'utf-8'
     import re
+    resp = requests.get(url=url)
+    resp.encoding = "utf-8"
+    soup = BeautifulSoup(resp.text)
     pixnet_content = soup.find("div", id="article-content-inner")
     removes = pixnet_content.find_all("script")
     for remove in removes:
@@ -25,12 +30,15 @@ def get_pixnet_content(soup):
     return pixnet_content
 
 
-def get_pixnet_image_urls(soup):
+def get_pixnet_image_urls(url):
 
     # 傳入 BeautifulSoup 處理後的 soup
 
     # 此方法回傳 Urls List
     import re
+    resp = requests.get(url=url)
+    resp.encoding = "utf-8"
+    soup = BeautifulSoup(resp.text)
     pixnet_content = soup.find("div", id="article-content-inner")
     images_url = []
     pattern = r"http\://farm2*"
@@ -42,12 +50,15 @@ def get_pixnet_image_urls(soup):
     return images_url
 
 
-def get_candicecity_content(soup):
+def get_candicecity_content(url):
 
     # 傳入 經過BeautifulSoup 處理的 soup
     # 若出現亂碼情況 請在requests.get()之後
     # 加入encoding = 'utf-8'
     import re
+    resp = requests.get(url=url)
+    resp.encoding = "utf-8"
+    soup = BeautifulSoup(resp.text)
     content = soup.find("div", class_="entry-content")
     clean_content = ""
     removes = content.find_all("ins")
@@ -78,8 +89,11 @@ def get_candicecity_content(soup):
     return clean_content
 
 
-def get_candicecity_images_url(soup):
+def get_candicecity_images_url(url):
     import re
+    resp = requests.get(url=url)
+    resp.encoding = "utf-8"
+    soup = BeautifulSoup(resp.text)
     content = soup.find("div", class_="entry-content")
     pattern = r"//candicecity*"
     images_url = []
@@ -91,8 +105,11 @@ def get_candicecity_images_url(soup):
     return images_url
 
 
-def get_lanlan_content(soup):
+def get_lanlan_content(url):
     import re
+    resp = requests.get(url=url)
+    resp.encoding = "utf-8"
+    soup = BeautifulSoup(resp.text)
     content = soup.find("div", class_="entry-content")
     removes = content.find_all("script")
     for remove in removes:
@@ -110,8 +127,11 @@ def get_lanlan_content(soup):
     return content
 
 
-def get_lanlan_images_url(soup):
+def get_lanlan_images_url(url):
     import re
+    resp = requests.get(url=url)
+    resp.encoding = "utf-8"
+    soup = BeautifulSoup(resp.text)
     pattern = r"https?\://image\.lanlan\.tw/[^emotion]"
     content = soup.find("div", class_="entry-content")
     images = content.find_all("img")
@@ -123,8 +143,11 @@ def get_lanlan_images_url(soup):
     return images_url
 
 
-def get_vivawei_content(soup):
+def get_vivawei_content(url):
     import re
+    resp = requests.get(url=url)
+    resp.encoding = "utf-8"
+    soup = BeautifulSoup(resp.text)
     content = soup.find("div", class_="desc")
     removes = content.find_all("script")
     for remove in removes:
@@ -143,8 +166,11 @@ def get_vivawei_content(soup):
     return content
 
 
-def get_vivawei_images_url(soup):
+def get_vivawei_images_url(url):
     import re
+    resp = requests.get(url=url)
+    resp.encoding = "utf-8"
+    soup = BeautifulSoup(resp.text)
     content = soup.find("div", class_="desc")
     images = content.find_all("img")
     images_url = []
